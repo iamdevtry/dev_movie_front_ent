190479f4bebd1269dd597ffc0961b9306b33eeb5
Binh Phong Tran <69306217+iamdevtry@users.noreply.github.com>
2023-02-18 17:54:26 +0700

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '9cc294ead579f40fe8b6815c182d9469',
+    apiKey: 'YOUR-API_KEY',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
